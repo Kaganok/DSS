dd86cc66e1b9e267a3111a7e09a4825004fb65db
Kaganok <105423029+Kaganok@users.noreply.github.com>
2025-10-20 12:20:52 +0300

    Add files via upload

---
+++ Week 1/1_practice_full.sql
@@ -0,0 +1,7 @@
+
+CREATE TABLE Sales (Month TEXT, Sales INTEGER);
+INSERT INTO Sales VALUES ('Jan',12000),('Feb',15000),('Mar',17000),('Apr',16000),('May',18000);
+
+CREATE TABLE Patients (PatientID INT, Symptoms TEXT, TestResult TEXT);
+INSERT INTO Patients VALUES (1,'Fever, Cough','Positive'),(2,'Headache, Nausea','Negative'),
+(3,'Cough, Fatigue','Positive'),(4,'Chest Pain','Negative');
